b74274cc149561e70bf5d461e116d1450a973cc2
John Park <ohrak22@gmail.com>
2017-07-21 14:54:00 +0800

    Create Xamarin project

---
+++ My2048/My2048/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace My2048
+{
+	[Activity(Label = "My2048", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Black.NoTitleBar")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
